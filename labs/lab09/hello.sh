012ef0c23d18b86611a87ac3bfb74144c66b35f1
carmelmuzhunga <1032215798@pfur.ru>
2023-04-04 05:36:58 -0700

    lab 9

---
+++ labs/lab09/hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+HELLO=Hello
+function hello {
+local HELLO=World
+echo $HELLOecho $HELLOecho $HELLOecho $HELLO
+}
+echo $HELLO
+hello
+local HELLO=World
+echo $HELLOecho $HELLOecho $HELLOecho $HELLO
+}
+echo $HELLO
+hello
+local HELLO=World
+echo $HELLOecho $HELLOecho $HELLOecho $HELLO
+}
+echo $HELLO
+hello
